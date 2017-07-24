40d171c77f238bb7593d530fa7379262b2b88970
Owen Campbell <owen.campbell@tanti.org.uk>
2017-07-24 21:12:53 +0100

    Add test for titfortat

--- tests/test_axelrod_fortran.py
+++ tests/test_axelrod_fortran.py
@@ -1,5 +1,12 @@
-import axelrod_fortran
+from axelrod_fortran.player import Player
+import axelrod as axl
+from axelrod.action import Action
+
+C, D = Action.C, Action.D
 
 
-def test_main():
-    assert axelrod_fortran  # use your library here
+def test_titfortat():
+    players = (Player('ktitfortatc_'), axl.Alternator())
+    match = axl.Match(players, 5)
+    expected = [(C, C), (C, D), (D, C), (C, D), (D, C)]
+    assert match.play() == expected
