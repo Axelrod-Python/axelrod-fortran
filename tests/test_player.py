86b64e1d9cb37e99754f71e16967740da8f92a7c
Owen Campbell <owen.campbell@tanti.org.uk>
2017-07-25 13:31:18 +0100

    Add tests for player init

---
+++ tests/test_player.py
@@ -0,0 +1,10 @@
+from axelrod_fortran.strategies import all_strategies
+from axelrod_fortran.player import Player
+from ctypes import c_int
+
+
+def test_init():
+    for strategy in all_strategies:
+        player = Player(strategy)
+        assert player.original_name == strategy
+        assert player.original_function.restype == c_int
