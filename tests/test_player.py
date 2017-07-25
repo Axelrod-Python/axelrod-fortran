b34035e4d81e4cb659a848c879a134e268b11059
Owen Campbell <owen.campbell@tanti.org.uk>
2017-07-25 17:10:59 +0100

    Add with statement to control lifetime of player object

--- tests/test_player.py
+++ tests/test_player.py
@@ -36,12 +36,12 @@ def test_matches():
     move_number=integers(min_value=1, max_value=200),
     my_score=integers(min_value=0, max_value=200),
     their_score=integers(min_value=0, max_value=200),
-    )
+)
 def test_original_strategy(move_number, my_score, their_score):
     for their_last_move, my_last_move in itertools.product((0, 1), repeat=2):
         for strategy in all_strategies:
-            player = Player(strategy)
-            action = player.original_strategy(
-                their_last_move, move_number, my_score, their_score, 0,
-                my_last_move)
-            assert action in (0, 1), print(f'{strategy} returned {action}')
+            with Player(strategy) as player:
+                action = player.original_strategy(
+                    their_last_move, move_number, my_score, their_score, 0,
+                    my_last_move)
+                assert action in (0, 1), print(f'{strategy} returned {action}')
