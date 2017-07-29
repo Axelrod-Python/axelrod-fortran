8aa18b704eb71014b3170cceed89050ebfe2c00e
Owen Campbell <owen.campbell@empiria.co.uk>
2017-07-29 16:25:04 +0100

    [#36] Simplify test code

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -7,12 +7,4 @@ def test_original_rank():
         for details in characteristics.values()
         if details['original_rank'] is not None]
 
-    assert len(ranks) == 63
-
-    # Test that are no duplicates in the rank values
-    assert len(ranks) == len(set(ranks))
-
-    # There were 63 strategies in the second tournament and so every integer
-    # between 1 and 63 inclusive should appear in the ranks list. Those values
-    # sum to 2016.
-    assert sum(ranks) == 2016
+    assert sorted(ranks) = list(range(1, 63 + 1))
