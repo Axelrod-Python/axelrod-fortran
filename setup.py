2fe68523292037eb01330cb7e51176e7c8df8b74
Owen Campbell <owen.campbell@empiria.co.uk>
2017-07-30 10:48:50 +0100

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*names, **kwargs):
 
 setup(
     name='axelrod-fortran',
-    version='0.1.0',
+    version='0.2.0',
     license='MIT license',
     description='Python wrapper for strategies originally written in Fortran',
     long_description='%s\n%s' % (
--- src/axelrod_fortran/__init__.py
+++ src/axelrod_fortran/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .player import Player
 from .strategies import (all_strategies, characteristics,
